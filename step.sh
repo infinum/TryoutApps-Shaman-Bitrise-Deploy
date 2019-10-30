822762edc7b590dc2ebdc03d264eaca50fcb6e3f
Jasmin Abou Aldan <jasmin.aboualdan@infinum.hr>
2019-10-30 11:47:34 +0100

    Initial deploy implementation

--- step.sh
+++ step.sh
@@ -3,17 +3,15 @@ set -ex
 
 echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+# gem install shaman_cli
+shaman -v
+PARAMS="${environment_name}" -t -c "${shaman_config_path}" -f "${file_path}"
+
+if [[ ! "${changelog_message}" ]]; then
+	CHANGELOG="${changelog_message}"
+fi
+
+shaman deploy "$PARAMS$CHANGELOG"
 
 #
 # --- Exit codes:
